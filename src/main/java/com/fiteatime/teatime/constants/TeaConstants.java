694265db1a72f233cf89f46cf5b4209f82806caa
fitri <margiawanfitriani@u.boisestate.edu>
2018-11-02 11:06:13 -0600

    constant

---
+++ src/main/java/com/fiteatime/teatime/constants/TeaConstants.java
@@ -0,0 +1,7 @@
+package com.fiteatime.teatime.constants;
+
+public class TeaConstants {
+    public static final String RESOURCE_ID = "resourceId";
+    public static final String TEA_TYPE = "teaType";
+    public static final String TEA_NAME = "teaName";
+}
